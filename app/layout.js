f68f42c099a5afd880a4052dfd23cb5aacefb87d
Daniel Van Bueren <daniel.vanbueren@proton.me>
2026-07-10 11:37:11 -0500

    Add icon

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,18 @@ import {StorageProvider} from "@/context/StorageContext";
 export const metadata = {
     title: 'Retirement Calculator',
     description: 'US Military Pay and Retirement Calculator',
+    icons: {
+        icon: [
+            { url: '/icon/icon0.svg', type: 'image/svg+xml' },
+            { url: '/icon/icon1.png', type: 'image/png' },
+        ],
+        shortcut: '/icon/favicon.ico',
+        apple: '/icon/apple-icon.png',
+    },
+    manifest: '/icon/manifest.json',
+    appleWebApp: {
+        title: 'Retirement Calculator',
+    },
 };
 
 export default function RootLayout({ children }) {
